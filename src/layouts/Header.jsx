6fc2a51f67daabe5773d23046e1e938d0cd0027d
ChuckBDT <charlesboudrant@protonmail.com>
2023-06-10 16:35:52 +0200

    Header added

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+import Logo from "../assets/Logo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='h-20 px-6 sm:px-10 grid grid-cols-3 justify-items-center items-center'>
+      {
+        <NavLink to='/catalog' className='text-start w-full '>
+          Browse the catalog
+        </NavLink>
+      }
+      {
+        <Link to='/'>
+          <img src={Logo} alt='BrewDog Logo' className='h-16' />
+        </Link>
+      }
+      <a href='https://punkapi.com' className='w-full text-end'>
+        Punk API
+      </a>
+    </header>
+  );
 };
 
 export default Header;
