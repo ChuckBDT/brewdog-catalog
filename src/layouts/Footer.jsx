999d7f66c790333e8985f78cedb0f1f68c7adb3b
ChuckBDT <charlesboudrant@protonmail.com>
2023-06-10 17:31:11 +0200

    Home page added

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-gray-100 w-full h-10 fixed bottom-0 flex justify-center items-center'>
+      <p>
+        Made with &#10084; by <a href='https://github.com/ChuckBDT'>ChuckBDT</a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import homeHeroSm from "../assets/hero/HomeHero-sm.webp";
+import homeHeroMd from "../assets/hero/HomeHero-md.webp";
+import homeHeroLg from "../assets/hero/HomeHero-lg.webp";
+import homeHeroXl from "../assets/hero/HomeHero-xl.webp";
+import homeHeroFull from "../assets/hero/HomeHero-full.webp";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <img
+        src={homeHeroSm}
+        srcSet={`${homeHeroSm} 320w, ${homeHeroMd} 640w, ${homeHeroLg} 1024w, ${homeHeroXl} 1280w, ${homeHeroFull} 1920w `}
+        className='w-full h-72 2xl:h-auto object-cover object-right-bottom'
+        alt=''
+      />
+    </main>
+  );
 };
 
 export default Home;
