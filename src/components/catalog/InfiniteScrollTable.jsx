fc5b86c0880581cc8ff8df83cc404f0f8cccd1d9
ChuckBDT <charlesboudrant@protonmail.com>
2023-06-17 12:54:12 +0200

    Infinite scrolling added to the catalog

--- src/components/catalog/InfiniteScrollTable.jsx
+++ src/components/catalog/InfiniteScrollTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
 const InfiniteScrollTable = ({
@@ -9,21 +9,30 @@ const InfiniteScrollTable = ({
   setCallPage,
 }) => {
   const [beers, setBeers] = useState([]);
+  const observer = useRef();
+  const lastBeerElRef = useCallback(
+    (node) => {
+      if (isLoading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          if (data.length === 20) {
+            setCallPage(callPage + 1);
+          }
+        }
+      });
+      if (node) observer.current.observe(node);
+      console.log(node);
+    },
+    [data, isLoading]
+  );
   const columnsKey = Object.keys(headOrder);
-
-  const handleNextPage = () => {
-    if (data.length === 20) {
-      setCallPage(callPage + 1);
-    } else {
-      console.log('End of data');
-    }
-  };
 
   useEffect(() => {
     data.forEach((item) => {
       setBeers((prevstate) => [...prevstate, item]);
     });
-  }, [data]);
+  }, [data, isLoading]);
 
   return (
     <>
@@ -39,34 +48,66 @@ const InfiniteScrollTable = ({
           </tr>
         </thead>
         <tbody className="">
-          {beers.map((item, index) => (
-            <tr key={index} className="">
-              {columnsKey.map((head, i) => (
-                <td key={i} className="px-4 h-10">
-                  {item[head]}
+          {beers.map((item, index) => {
+            if (beers.length === index + 1) {
+              return (
+                <tr key={index} ref={lastBeerElRef} className="">
+                  {columnsKey.map((head, i) => (
+                    <td key={i} className="px-4 h-10">
+                      {item[head]}
+                    </td>
+                  ))}
+                  <td className="px-4 h-10 flex justify-center  items-center">
+                    <Link to={`/beers/${item.id}`} state={{ item }}>
+                      <svg
+                        className="hover:w-6 hover:h-6 transition-all"
+                        stroke="currentColor"
+                        fill="currentColor"
+                        strokeWidth="0"
+                        viewBox="0 0 16 16"
+                        height="1.2em"
+                        width="1.2em"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                        ></path>
+                      </svg>
+                    </Link>
+                  </td>
+                </tr>
+              );
+            }
+            return (
+              <tr key={index} className="">
+                {columnsKey.map((head, i) => (
+                  <td key={i} className="px-4 h-10">
+                    {item[head]}
+                  </td>
+                ))}
+                <td className="px-4 h-10 flex justify-center  items-center">
+                  <Link to={`/beers/${item.id}`} state={{ item }}>
+                    <svg
+                      className="hover:w-6 hover:h-6 transition-all"
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 16 16"
+                      height="1.2em"
+                      width="1.2em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                      ></path>
+                    </svg>
+                  </Link>
                 </td>
-              ))}
-              <td className="px-4 h-10 flex justify-center  items-center">
-                <Link to={`/beers/${item.id}`} state={{ item }}>
-                  <svg
-                    className="hover:w-6 hover:h-6 transition-all"
-                    stroke="currentColor"
-                    fill="currentColor"
-                    strokeWidth="0"
-                    viewBox="0 0 16 16"
-                    height="1.2em"
-                    width="1.2em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
-                    ></path>
-                  </svg>
-                </Link>
-              </td>
-            </tr>
-          ))}
+              </tr>
+            );
+          })}
           {isLoading && (
             <tr className=" h-10">
               {columnsKey.map((i) => (
