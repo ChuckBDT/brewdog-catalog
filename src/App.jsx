97e55fad84c9dbe7976ccced4d9f88add27bad4a
ChuckBDT <charlesboudrant@pm.me>
2024-11-29 11:22:29 +0100

    Duplicate mockedData and add warn message about PunkAPI down

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <div className="w-full py-2 px-4 text-center bg-yellow-300">As Punk API is down, the app is now using a set of 20 mocked beers i kept apart from the original data. Images and content may (will) not be accurate.</div>
       <main className="bg-gray-50 flex-1 flex px-8">
         <Outlet />
       </main>
